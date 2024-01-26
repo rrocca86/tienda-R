b9bdf1a11d11b02dfaa16ba6d67b7c1adb766ab8
rrocca86 <renzorocca86@gmail.com>
2024-01-26 17:19:57 -0300

    Se agrega animación spinning

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,39 +1,49 @@
-import { React, useState, useEffect } from 'react'
-import Item from '../Item/Item'
-import "./ItemList.css"
+import { React, useState, useEffect } from "react";
+import Item from "../Item/Item";
+import "./ItemList.css";
 import { getItems } from "../../../public/data";
+import { useLoading, Grid } from "@agney/react-loading";
 
 const ItemList = () => {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                console.log("Trying to get items")
-                const itemsData = await getItems();
-                setItems(itemsData);
-            } catch (error) {
-                console.error('Error fetching items', error);
-            }
-        };
+  const { containerProps, indicatorEl } = useLoading({
+    loading: true,
+    indicator: <Grid width="50" />,
+  });
 
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("Trying to get items");
+        const itemsData = await getItems();
+        setItems(itemsData);
+      } catch (error) {
+        console.error("Error fetching items", error);
+      }
+    };
 
-    return (
-        <>
-            <div className='container'>
-                {items.length == 0
-                    ?
-                    <h1>CARGANDO..</h1>
-                    :
-                    <div className="columns is-multiline">
-                        {items.map((p) => <Item key={p.id} {...p} />)}
-                    </div>
-                }
-            </div>
-        </>
-    )
-}
+    fetchData();
+  }, []);
 
-export default ItemList
+  return (
+    <>
+      <div className="container">
+        {items.length == 0 ? (
+          <section {...containerProps}>
+            {indicatorEl} {/* renders only while loading */}
+            <h1>Cargando</h1>
+          </section>
+        ) : (
+          <div className="columns is-multiline">
+            {items.map((p) => (
+              <Item key={p.id} {...p} />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ItemList;
