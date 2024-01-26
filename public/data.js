d2a7115d626d3a170ed4bff8bbae75f09b5e6d0d
rrocca86 <renzorocca86@gmail.com>
2024-01-26 16:06:32 -0300

    Se agregan mas productos a Data.js.

--- public/data.js
+++ public/data.js
@@ -1,72 +1,153 @@
 const Data = [
-    {
-        "category": "men's clothing",
-        "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-        "id": 3,
-        "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-        "price": 55.99,
-        "rating": {
-            "count": 500,
-            "rate": 4.7
-        },
-        "title": "Mens Cotton Jacket"
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: {
+      rate: 3.9,
+      count: 120,
     },
-    {
-        "category": "men's clothing",
-        "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-        "id": 4,
-        "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-        "price": 15.99,
-        "rating": {
-            "count": 430,
-            "rate": 2.1
-        },
-        "title": "Mens Casual Slim Fit"
+    stock: 50,
+  },
+  {
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts ",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    category: "men's clothing",
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    rating: {
+      rate: 4.1,
+      count: 259,
     },
-    {
-        "category": "women's clothing",
-        "description": "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
-        "id": 18,
-        "image": "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
-        "price": 9.85,
-        "rating": {
-            "count": 130,
-            "rate": 4.7
-        },
-        "title": "MBJ Women's Solid Short Sleeve Boat Neck V "
+    stock: 41,
+  },
+  {
+    category: "men's clothing",
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    id: 3,
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    price: 55.99,
+    rating: {
+      count: 500,
+      rate: 4.7,
     },
-    {
-        "category": "women's clothing",
-        "description": "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
-        "id": 19,
-        "image": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
-        "price": 7.95,
-        "rating": {
-            "count": 146,
-            "rate": 4.5
-        },
-        "title": "Opna Women's Short Sleeve Moisture"
+    title: "Mens Cotton Jacket",
+    stock: 7,
+  },
+  {
+    category: "men's clothing",
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    id: 4,
+    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    price: 15.99,
+    rating: {
+      count: 430,
+      rate: 2.1,
     },
-    {
-        "category": "women's clothing",
-        "description": "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-        "id": 20,
-        "image": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
-        "price": 12.99,
-        "rating": {
-            "count": 145,
-            "rate": 3.6
-        },
-        "title": "DANVOUY Womens T Shirt Casual Cotton Short"
-    }
-]
+    title: "Mens Casual Slim Fit",
+    stock: 12,
+  },
+  {
+    category: "women's clothing",
+    description:
+      "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+    id: 18,
+    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    price: 9.85,
+    rating: {
+      count: 130,
+      rate: 4.7,
+    },
+    title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
+    stock: 8,
+  },
+  {
+    id: 5,
+    title:
+      "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    price: 695,
+    description:
+      "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    rating: {
+      rate: 4.6,
+      count: 400,
+    },
+  },
+  {
+    id: 6,
+    title: "Solid Gold Petite Micropave ",
+    price: 168,
+    description:
+      "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    rating: {
+      rate: 3.9,
+      count: 70,
+    },
+    stock: 2,
+  },
+  {
+    id: 7,
+    title: "White Gold Plated Princess",
+    price: 9.99,
+    description:
+      "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    rating: {
+      rate: 3,
+      count: 400,
+    },
+    stock: 36,
+  },
+  {
+    category: "women's clothing",
+    description:
+      "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+    id: 19,
+    image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    price: 7.95,
+    rating: {
+      count: 146,
+      rate: 4.5,
+    },
+    title: "Opna Women's Short Sleeve Moisture",
+    stock: 22,
+  },
+  {
+    category: "women's clothing",
+    description:
+      "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    id: 20,
+    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    price: 12.99,
+    rating: {
+      count: 145,
+      rate: 3.6,
+    },
+    title: "DANVOUY Womens T Shirt Casual Cotton Short",
+    stock: 3,
+  },
+];
 
 export default Data;
 
 export const getItems = () => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(Data);
-        }, 200);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Data);
+    }, 200);
+  });
 };
