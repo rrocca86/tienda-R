530e43ccc3b5faa5dcbb0ed91d111d9281e7186c
rrocca86 <renzorocca86@gmail.com>
2024-01-30 15:19:05 -0300

    Se separan funcionalidades entre ItemList e ItemListContainer

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,46 +1,16 @@
-import { React, useState, useEffect } from "react";
+import React from "react";
 import Item from "../Item/Item";
 import "./ItemList.css";
-import { getItems } from "../../../public/data";
-import { useLoading, Grid } from "@agney/react-loading";
 
-const ItemList = () => {
-  const [items, setItems] = useState([]);
-
-  const { containerProps, indicatorEl } = useLoading({
-    loading: true,
-    indicator: <Grid width="50" />,
-  });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log("Trying to get items");
-        const itemsData = await getItems();
-        setItems(itemsData);
-      } catch (error) {
-        console.error("Error fetching items", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+const ItemList = ({ items }) => {
   return (
     <>
       <div className="container">
-        {items.length == 0 ? (
-          <section {...containerProps}>
-            {indicatorEl}
-            <h1>Cargando</h1>
-          </section>
-        ) : (
-          <div className="columns is-multiline">
-            {items.map((p) => (
-              <Item key={p.id} {...p} />
-            ))}
-          </div>
-        )}
+        <div className="columns is-multiline">
+          {items.map((p) => (
+            <Item key={p.id} {...p} />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,43 @@
 import ItemList from "../ItemList/ItemList";
+import { getItems } from "../../../public/data";
+import { useLoading, Grid } from "@agney/react-loading";
+import { React, useState, useEffect } from "react";
 
 const ItemListContainer = ({ greeting }) => {
-    return (
-        <div>
-            <h3 className="title is-4 has-text-centered mt-5">{greeting}</h3>
-            <ItemList />
-        </div>
-    )
-}
+  const [items, setItems] = useState([]);
+
+  const { containerProps, indicatorEl } = useLoading({
+    loading: true,
+    indicator: <Grid width="50" />,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("Trying to get items");
+        const itemsData = await getItems();
+        setItems(itemsData);
+      } catch (error) {
+        console.error("Error fetching items", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h3 className="title is-4 has-text-centered mt-5">{greeting}</h3>
+      {items.length == 0 ? (
+        <section {...containerProps}>
+          {indicatorEl}
+          <h1>Cargando</h1>
+        </section>
+      ) : (
+        <ItemList items={items} />
+      )}
+    </div>
+  );
+};
 
 export default ItemListContainer;
