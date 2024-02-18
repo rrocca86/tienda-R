b1bc6dfca70d197d6eeed6b85426388662b292e5
rrocca86 <renzorocca86@gmail.com>
2024-02-18 15:55:52 -0300

    Se agrega navilidad a icono del carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,13 +2,16 @@ import React, { useContext } from "react";
 import cart from "./assets/cart-shopping-solid.svg";
 import "./CartWidget.css";
 import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
   const { totalQuantity } = useContext(CartContext);
 
   return (
     <div className="navbar-item">
-      <img src={cart} alt="" height="30" width="30" />{" "}
+      <Link to="/cart">
+        <img src={cart} alt="" height="30" width="30" />{" "}
+      </Link>
       <p>{totalQuantity == null ? 0 : totalQuantity}</p>
     </div>
   );
